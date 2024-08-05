8cd499ad25c381ea0685b3383d7683a09f37a5cc
Abdullah <abdullah.khan4620@gmail.com>
2024-08-05 14:19:13 -0700

    Added request id parameter

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,7 @@ import json
 from asyncio import sleep
 import base64
 import matplotlib
+
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import io
@@ -16,21 +17,47 @@ import networkx as nx
 from networkx.drawing.nx_agraph import graphviz_layout
 
 
-cols = ['A Feed', 'D Feed', 'E Feed', 'A and C Feed', 'Recycle Flow',
-    'Reactor Feed Rate', 'Reactor Pressure', 'Reactor Level',
-    'Reactor Temperature', 'Purge Rate', 'Product Sep Temp',
-    'Product Sep Level', 'Product Sep Pressure', 'Product Sep Underflow',
-    'Stripper Level', 'Stripper Pressure', 'Stripper Underflow',
-    'Stripper Temp', 'Stripper Steam Flow', 'Compressor Work',
-    'Reactor Coolant Temp', 'Separator Coolant Temp', 'D feed load', 'E feed load', 'A feed load',
-    'A and C feed load', 'Compressor recycle valve', 'Purge valve',
-    'Separator liquid load', 'Stripper liquid load', 'Stripper steam valve',
-    'Reactor coolant load', 'Condenser coolant load']
-mapping = {f"x{idx+1}":c for idx, c in enumerate(cols)}
+cols = [
+    "A Feed",
+    "D Feed",
+    "E Feed",
+    "A and C Feed",
+    "Recycle Flow",
+    "Reactor Feed Rate",
+    "Reactor Pressure",
+    "Reactor Level",
+    "Reactor Temperature",
+    "Purge Rate",
+    "Product Sep Temp",
+    "Product Sep Level",
+    "Product Sep Pressure",
+    "Product Sep Underflow",
+    "Stripper Level",
+    "Stripper Pressure",
+    "Stripper Underflow",
+    "Stripper Temp",
+    "Stripper Steam Flow",
+    "Compressor Work",
+    "Reactor Coolant Temp",
+    "Separator Coolant Temp",
+    "D feed load",
+    "E feed load",
+    "A feed load",
+    "A and C feed load",
+    "Compressor recycle valve",
+    "Purge valve",
+    "Separator liquid load",
+    "Stripper liquid load",
+    "Stripper steam valve",
+    "Reactor coolant load",
+    "Condenser coolant load",
+]
+mapping = {f"x{idx+1}": c for idx, c in enumerate(cols)}
 
 G = nx.read_adjlist("./cg.adjlist", create_using=nx.DiGraph)
 # G = nx.reverse(G)
 G = nx.relabel_nodes(G, mapping)
+
 
 def get_subgraph(G, nodes):
     subgraph_nodes = set(nodes)
@@ -69,32 +96,32 @@ Condensed components move to a product stripping column to remove remaining reac
 Products G and H exit the stripper base and are separated in a downstream refining section which is not included in this problem.
 The inert and byproduct are primarily purged from the system as a vapor from the vapor-liquid separator."""
 
-SYSTEM_MESSAGE = 'You are a helpful AI chatbot trained to assist with '\
-    'monitoring the Tennessee Eastman process. The Tennessee Eastman '\
-    f'Process can be described as follows:\n{INTRO_MESSAGE}'\
-    '\n\nYour purpose is to help the user identify and understand potential '\
-    'explanations for any faults that occur during the process. You should '\
-    'explain your reasoning using the graphs provided.'
+SYSTEM_MESSAGE = (
+    "You are a helpful AI chatbot trained to assist with "
+    "monitoring the Tennessee Eastman process. The Tennessee Eastman "
+    f"Process can be described as follows:\n{INTRO_MESSAGE}"
+    "\n\nYour purpose is to help the user identify and understand potential "
+    "explanations for any faults that occur during the process. You should "
+    "explain your reasoning using the graphs provided."
+)
 
-EXPLAIN_PROMPT = 'You are provided with the general schematics of the Tennessee'\
-    'Eastman process, causal graphs of different features and graphs showing '\
-    'the values of the top contributing features for a recent fault. For every '\
-    'contributing feature reason about the observation graphs (not all '\
-    'contributing features might have sudden change around the fault) and '\
-    'create hypotheses for these observations based on the causal graph. '\
-    'Finally combine these hypotheses in order to generate an explanation as to'\
-    'why this fault occurred and how it is propagating.'
+EXPLAIN_PROMPT = (
+    "You are provided with the general schematics of the Tennessee"
+    "Eastman process, causal graphs of different features and graphs showing "
+    "the values of the top contributing features for a recent fault. For every "
+    "contributing feature reason about the observation graphs (not all "
+    "contributing features might have sudden change around the fault) and "
+    "create hypotheses for these observations based on the causal graph. "
+    "Finally combine these hypotheses in order to generate an explanation as to"
+    "why this fault occurred and how it is propagating."
+)
 
 client = OpenAI()
 
 # Initialize FastAPI app
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:5173",
-    "*"
-]
+origins = ["http://localhost", "http://localhost:5173", "*"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -104,16 +131,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 # Define request and response models
 class MessageRequest(BaseModel):
     data: list[dict[str, str]]
+    id: str
+
 
 class ExplainationRequest(BaseModel):
     data: dict[str, list[float]]
+    id: str
+
 
 class Image(BaseModel):
     image: str
     name: str
+
 
 class MessageResponse(BaseModel):
     content: str
@@ -121,18 +154,19 @@ class MessageResponse(BaseModel):
     index: int
     id: str
 
-def ChatModelCompletion(messages: list[dict[str, str]], images: list[str]=None):
+
+def ChatModelCompletion(
+    messages: list[dict[str, str]], msg_id: str, images: list[str] = None
+):
     # Send the message to OpenAI's GPT-4
     # print(messages)
     response = client.chat.completions.create(
         model="gpt-4o",
-        # messages=[
-        #     {"role": "user", "content": toModel}
-        # ],
         messages=messages,
-        stream=True
+        stream=True,
+        temperature=0,
     )
-    print('sending response')
+    print("sending response")
     index = 0
     for chunk in response:
         # print(chunk)
@@ -140,33 +174,57 @@ def ChatModelCompletion(messages: list[dict[str, str]], images: list[str]=None):
         if chunk.choices[0].delta.content:
             response_text = chunk.choices[0].delta.content
             if index == 0 and images:
-                response_str = json.dumps({'index': index, 'content': response_text, 'id': chunk.id, 'images': images})
+                response_str = json.dumps(
+                    {
+                        "index": index,
+                        "content": response_text,
+                        "id": msg_id,
+                        "images": images,
+                    }
+                )
             else:
-                response_str = json.dumps({'index': index, 'content': response_text, 'id': chunk.id, 'images': []})
+                response_str = json.dumps(
+                    {
+                        "index": index,
+                        "content": response_text,
+                        "id": msg_id,
+                        "images": [],
+                    }
+                )
             index += 1
             yield "data: " + response_str + "\n\n"
-    print(f'Sent {index} chunks')
+    print(f"Sent {index} chunks")
+
 
 def plot_causal_subgraph(request: ExplainationRequest):
     nodes_of_interest = request.data.keys()
     subgraph = get_subgraph(G, nodes_of_interest)
     # Visualize the subgraph
-    pos = graphviz_layout(subgraph, prog='dot')
-    nx.draw(subgraph, pos, with_labels=True, node_color='lightblue',
-            node_size=500, font_size=10, arrows=True)
-    nx.draw_networkx_nodes(subgraph, pos, nodelist=nodes_of_interest,
-                        node_color='red', node_size=600)
+    pos = graphviz_layout(subgraph, prog="dot")
+    nx.draw(
+        subgraph,
+        pos,
+        with_labels=True,
+        node_color="lightblue",
+        node_size=500,
+        font_size=10,
+        arrows=True,
+    )
+    nx.draw_networkx_nodes(
+        subgraph, pos, nodelist=nodes_of_interest, node_color="red", node_size=600
+    )
     plt.title("Causal graph of important features (higlighted in red)")
-    plt.axis('off')
+    plt.axis("off")
     img_bytes = io.BytesIO()
     plt.savefig(img_bytes, format="png")
     img_bytes.seek(0)
     img_base64 = base64.b64encode(img_bytes.read()).decode()
-    # DEBUG
-    with open(f"./img/causal_graph.png", "wb") as f:
-        f.write(base64.b64decode(bytes(img_base64, "utf-8")))
+    # # DEBUG
+    # with open(f"./img/causal_graph.png", "wb") as f:
+    #     f.write(base64.b64decode(bytes(img_base64, "utf-8")))
     plt.close()
     return {"image": img_base64, "name": "Causal graph"}
+
 
 def plot_graphs_to_base64(request: ExplainationRequest):
     graphs = []
@@ -174,24 +232,21 @@ def plot_graphs_to_base64(request: ExplainationRequest):
         try:
             # Plot the feature's historical data
             fig, ax = plt.subplots()
-            ax.step(range(len(request.data[feature_name])), request.data[feature_name], label=feature_name, where='mid')
+            ax.step(
+                range(len(request.data[feature_name])),
+                request.data[feature_name],
+                label=feature_name,
+                where="mid",
+            )
             # ax.plot(request.data[feature_name], label=feature_name)
 
             ax.axvline(
-                x=len(request.data[feature_name])-20,
+                x=len(request.data[feature_name]) - 20,
                 color="r",
                 linestyle="--",
                 label="Fault Start",
             )
 
-            # Format the x-axis to display dates correctly
-            # ax.xaxis.set_major_locator(mdates.AutoDateLocator())
-            # ax.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M:%S"))
-
-            # Rotate date labels for better readability
-            # plt.xticks(rotation=45)
-
-            # Set labels and title
             ax.set_xlabel("Time")
             ax.set_ylabel(feature_name)
             ax.set_title(f"{feature_name} over Time around Fault")
@@ -204,9 +259,9 @@ def plot_graphs_to_base64(request: ExplainationRequest):
             plt.savefig(img_bytes, format="png")
             img_bytes.seek(0)
             img_base64 = base64.b64encode(img_bytes.read()).decode()
-            # DEBUG
-            with open(f"./img/{feature_name}.png", "wb") as f:
-                f.write(base64.b64decode(bytes(img_base64, "utf-8")))
+            # # DEBUG
+            # with open(f"./img/{feature_name}.png", "wb") as f:
+            #     f.write(base64.b64decode(bytes(img_base64, "utf-8")))
             # Send the image to the frontend
             graphs.append({"name": feature_name, "image": img_base64})
             plt.close(fig)
@@ -214,81 +269,61 @@ def plot_graphs_to_base64(request: ExplainationRequest):
             print(e)
     return graphs
 
+
 @app.post("/explain", response_model=None)
 async def explain(request: ExplainationRequest):
-    # print(f'Received fault {request.data}')
     try:
         with open("./tep_flowsheet.png", "rb") as image_file:
             schematic_img_base64 = base64.b64encode(image_file.read()).decode("utf-8")
         graphs = plot_graphs_to_base64(request)
-        # with open("./cg.png", "rb") as image_file:
-        #     cg_base64 = base64.b64encode(image_file.read()).decode("utf-8")
         causal_graph = plot_causal_subgraph(request)
-        # causal_graph = {"image": cg_base64, "name": "Causal graph"}
         schema_image = {
             "type": "image_url",
-            "image_url": {
-                "url": f"data:image/png;base64,{schematic_img_base64}"
-            }
+            "image_url": {"url": f"data:image/png;base64,{schematic_img_base64}"},
         }
-        # image_data.append(
-        #     {
-        #         "type": "image_url",
-        #         "image_url": {"url": f"data:image/png;base64,{cg_base64}"},
-        #     }
-        # )
-        emessages=[
-            {
-                "role": "system",
-                "content": SYSTEM_MESSAGE
-            },
+        emessages = [
+            {"role": "system", "content": SYSTEM_MESSAGE},
             {
                 "role": "user",
                 "content": [
                     {"type": "text", "text": EXPLAIN_PROMPT},
                     schema_image,
-                ] + [
+                ]
+                + [
                     {
                         "type": "image_url",
                         "image_url": {"url": f"data:image/png;base64,{graph['image']}"},
                     }
                     for graph in graphs
-                ] + [
+                ]
+                + [
                     {
                         "type": "image_url",
-                        "image_url": {"url": f"data:image/png;base64,{causal_graph['image']}"}
+                        "image_url": {
+                            "url": f"data:image/png;base64,{causal_graph['image']}"
+                        },
                     }
-                ]
-            }
+                ],
+            },
         ]
-        return StreamingResponse(ChatModelCompletion(emessages, graphs+[causal_graph]), media_type='text/event-stream')
-        # return StreamingResponse(ChatModelCompletion(emessages, graphs), media_type='text/event-stream')
+        return StreamingResponse(
+            ChatModelCompletion(
+                messages=emessages,
+                msg_id=request.id,
+                images=graphs + [causal_graph],
+            ),
+            media_type="text/event-stream",
+        )
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
 
 @app.post("/send_message", response_model=MessageResponse)
 async def send_message(request: MessageRequest):
-    # print(f'Received a new request {request.data}')
     try:
-        # async def ChatModelCompletion(somestr):
-        #     for i in range(10):
-        #         yield "data: " + str(i) + "\n\n"
-        #         await sleep(0.5)
-        # STREAM: TRUE
-        return StreamingResponse(ChatModelCompletion(request.data), media_type='text/event-stream')
-        # # FALSE
-        # response = client.chat.completions.create(
-        #     model="gpt-4o",
-        #     messages=[
-        #         {"role": "user", "content": request.message}
-        #     ],
-        #     stream=False
-        # )
-        # response_text = response.choices[0].message.content
-        # return MessageResponse(response=response_text)
+        return StreamingResponse(
+            ChatModelCompletion(messages=request.data, msg_id=f"reply-{request.id}"),
+            media_type="text/event-stream",
+        )
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
